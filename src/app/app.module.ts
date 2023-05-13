c77af3e98608b0fb955232370d5854a1eeaebd49
ikrasnikov <krasenzhenya@gmail.com>
2023-05-13 19:49:35 +0300

    new deep component2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { routes } from './routes';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { DeepComponentComponent } from './deep-component/deep-component.component';
+import { DeepComponentComponent2 } from './deep-component2/deep-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    DeepComponentComponent
+    DeepComponentComponent,
+    DeepComponentComponent2
   ],
   imports: [
     BrowserModule,
